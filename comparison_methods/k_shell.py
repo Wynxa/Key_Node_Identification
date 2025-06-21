e945723c4bc67aeed28819282dce5aac23f12f92
Yuning <ningwin02@163.com>
2025-06-21 15:47:58 +0800

    k-shell去除自环

--- comparison_methods/k_shell.py
+++ comparison_methods/k_shell.py
@@ -3,5 +3,9 @@ import networkx as nx
 
 # K 壳分解算法
 def k_shell_method(G_nx, top_k=30):
+    G_nx = G_nx.copy()  # 避免修改原图
+    G_nx.remove_edges_from(nx.selfloop_edges(G_nx))  # 移除自环
     ks = nx.core_number(G_nx)
-    return sorted(ks, key=lambda x: -ks[x])[:top_k]
+    # 获取前 top_k 个核心数最大的节点
+    sorted_nodes = sorted(ks.items(), key=lambda x: x[1], reverse=True)
+    return [node for node, _ in sorted_nodes[:top_k]]
